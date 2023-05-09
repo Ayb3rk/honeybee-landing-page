0ff9d0cb879e044e0c9af363977e7649a497481d
ayberkgokmen <ayberk.gokmen@gmail.com>
2023-05-09 22:16:05 +0300

    fix grid

--- src/components/MiddleBlock/index.tsx
+++ src/components/MiddleBlock/index.tsx
@@ -1,7 +1,7 @@
 import { Row, Col } from "antd";
 import { withTranslation } from "react-i18next";
 import { Slide } from "react-awesome-reveal";
-import { MiddleBlockSection, Content, ContentWrapper, ServiceWrapper, MinTitle, MinPara, } from "./styles";
+import { MiddleBlockSection, ContentWrapper, ServiceWrapper, MinTitle, MinPara, } from "./styles";
 import { SvgIcon } from "../../common/SvgIcon";
 
 interface MiddleBlockProps {
@@ -13,22 +13,21 @@ interface MiddleBlockProps {
   t?: any;
 }
 
-const MiddleBlock = ({ title, content, section, t}: MiddleBlockProps) => {
+const MiddleBlock = ({ title, section}: MiddleBlockProps) => {
 
   return (
     <MiddleBlockSection>
       <Slide direction="up">
         <Row justify="center" align="middle">
           <ContentWrapper>
-            <Col lg={24} md={24} sm={12} xs={6}>
+            <Col lg={24} md={24} sm={24} xs={24}>
               <h6>{title}</h6>
-              <Content>{t(content)}</Content>
-              <ServiceWrapper>
+              <ServiceWrapper style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gridGap: "20px" }}>
                 <Row justify="space-between">
                   {typeof section === "object" &&
                     section.map((item: any, id: number) => {
                       return (
-                        <Col key={id} span={6}>
+                        <Col key={id} lg={6} md={6} sm={11} xs={24}>
                           <SvgIcon src={item.icon} width="60px" height="60px" />
                           <MinTitle>{item.title}</MinTitle>
                           <MinPara>{item.content}</MinPara>
